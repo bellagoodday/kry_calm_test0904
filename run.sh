c31abfc89819e7cb27a9301b0ea71751c529598f
Administrator <Administrator@CWTR8IIIW3VY7EO>
2017-09-26 23:06:22 +0800

    修改run.sh文件

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 echo "adb connect 127.0.0.1:62001"
 adb connect 127.0.0.1:62001
-echo "uninstall source apk"
-adb uninstall com.yuhj.ontheway
-echo "uninstall tested apk"
-adb uninstall com.yuhj.ontheway.test.test
 echo "start to install source apk"
 adb install apk\WelcomeActivity-debug.apk
 echo "start to install tested apk"
