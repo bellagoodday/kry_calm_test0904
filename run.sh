905fc21ed9b1d6f9526afe160e0a9320ac56ef11
Administrator <Administrator@CWTR8IIIW3VY7EO>
2017-09-26 16:53:56 +0800

    run.sh修改apk安装包路径

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ adb uninstall com.android.systemui.tests
 echo "uninstall tested apk"
 adb uninstall com.yuhj.ontheway.test.test
 echo "start to install source apk"
-adb install workspace\apk\WelcomeActivity-debug.apk
+adb install apk\WelcomeActivity-debug.apk
 echo "start to install tested apk"
-adb install workspace\bin\workspace-debug.apk
+adb install bin\workspace-debug.apk
 echo "start to run test"
 adb shell am instrument -w com.yuhj.ontheway.test.test/runner.Runner01
 
