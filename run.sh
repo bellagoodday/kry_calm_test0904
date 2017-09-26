de21aad61e1a2ec7ad5c9139b1b92f4adf14f290
Administrator <Administrator@CWTR8IIIW3VY7EO>
2017-09-26 23:25:26 +0800

    run.sh 加上根目录

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 echo "adb connect 127.0.0.1:62001"
 adb connect 127.0.0.1:62001
 echo "start to install source apk"
-adb install apk\WelcomeActivity-debug.apk
+adb install \apk\WelcomeActivity-debug.apk
 echo "start to install tested apk"
-adb install bin\workspace-debug.apk
+adb install \bin\workspace-debug.apk
 echo "start to run test"
 adb shell am instrument -w com.yuhj.ontheway.test.test/runner.Runner01
 
