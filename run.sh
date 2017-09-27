a530a1cc1391e57eb9ea2314151ee40bb2b383e7
Administrator <Administrator@CWTR8IIIW3VY7EO>
2017-09-27 09:46:20 +0800

    新增pwd

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "adb connect 127.0.0.1:62001"
 adb connect 127.0.0.1:62001
+pwd
 echo "start to install source apk"
 adb install workspace/apk/WelcomeActivity-debug.apk
 echo "start to install tested apk"
 adb install workspace/bin/workspace-debug.apk
 echo "start to run test"
 adb shell am instrument -w com.yuhj.ontheway.test.test/runner.Runner01
-
