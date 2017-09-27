a0b54da5af334f006833afaa0583056eca4257cc
Administrator <Administrator@CWTR8IIIW3VY7EO>
2017-09-27 11:16:21 +0800

    pwd ls

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ echo "adb connect 127.0.0.1:62001"
 adb connect 127.0.0.1:62001
 echo "pwd"
 pwd
+ls
 echo "start to install source apk"
 adb install workspace/apk/WelcomeActivity-debug.apk
 echo "start to install tested apk"
